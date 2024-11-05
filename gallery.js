f16c0737fd05f118ea70e0c8ecd9ca3530f3d274
sowassup <sowassup@gmail.com>
2024-11-04 20:50:13 -0800

    finished gallery

--- gallery.js
+++ gallery.js
@@ -1,8 +1,33 @@
-import Masonry from 'masonry-layout';
+document.addEventListener("DOMContentLoaded", function () {
+    id("title").textContent = "";
+    const grid = document.querySelector('#gallery-wall');
+    const paintings = document.querySelectorAll('.art img');
+    const masonry = new Masonry(grid, {
+      itemSelector: '.art',
+      columnWidth: 200,
+      gutter: 10
+    });
 
-const grid = document.querySelector('.gallery-wall');
-const masonry = new Masonry(grid, {
-  itemSelector: '.art',
-  columnWidth: 200,
-  gutter: 10
+    paintings.forEach(painting => {
+        painting.addEventListener('mouseover', function() {
+            let src = painting.src;
+            let fileName = src.substring(src.lastIndexOf('/') + 1);
+            fileName = decodeURIComponent(fileName).replace('.jpg', '');
+            id("title").textContent = fileName;
+        });
+    });
+
+
+    paintings.forEach(painting => {
+        painting.addEventListener('click', function() {
+            let src = painting.src;
+            let fileName = src.substring(src.lastIndexOf('/') + 1);
+            fileName = decodeURIComponent(fileName).replace('.jpg', '');
+            id("title").textContent = fileName;
+        });
+    });
+
+    function id(id) {
+        return document.getElementById(id);
+    }
 });
