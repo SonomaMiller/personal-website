aa50af35f7c339e3e9c5e8471ac3f79c1d723b53
sowassup <sowassup@gmail.com>
2024-12-10 12:55:26 -0800

    added spider, condensed font links

---
+++ portfolio.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("web").addEventListener("click", () => {
+    const spider = document.getElementById("spider");
+
+    // Set the GIF to visible and reset its opacity
+    spider.style.display = "block";
+    spider.style.opacity = 0;
+
+    // Fade in
+    let fadeIn = setInterval(() => {
+      if (spider.style.opacity < 1) {
+        spider.style.opacity = parseFloat(spider.style.opacity) + 0.1;
+      } else {
+        clearInterval(fadeIn);
+
+        // Wait for the GIF to play once, then fade out
+        const gifDuration = 2950;
+        setTimeout(() => {
+          let fadeOut = setInterval(() => {
+            if (spider.style.opacity > 0) {
+              spider.style.opacity = parseFloat(spider.style.opacity) - 0.1;
+            } else {
+              clearInterval(fadeOut);
+              spider.style.display = "none";
+            }
+          }, 50);
+        }, gifDuration);
+      }
+    }, 50);
+  });
+});
